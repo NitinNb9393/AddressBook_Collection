da052a8ba1928f532e2275f46b0ec0b0ad5dca02
NitinNb9393 <nitinbabar9393@gmail.com>
2021-11-18 14:37:10 +0530

    Added : Program for the Search person by the city or the state

---
+++ src/com/bridgelabz/address_book_collection/Sort.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.address_book_collection;
+
+import java.util.Comparator;
+
+public class Sort {
+	static Comparator<Contacts> compareCity = new Comparator<Contacts>() {
+		public int compare(Contacts one, Contacts two) {
+			return one.getCity().compareTo(two.getCity());
+		}
+	};
+
+}
